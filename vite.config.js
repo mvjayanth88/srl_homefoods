b63e9c584c920d7c0e349a27ad85f356f6182da5
Viswa Jayanth Matta <mvjayanth88@gmail.com>
2025-07-20 16:53:40 +0530

    fix: update base path in vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js (in root)
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/srl_homefoods/', // 👈 GitHub repo name
-})
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
